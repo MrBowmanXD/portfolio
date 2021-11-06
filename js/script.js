5bb047db750143c6ec22a55a419b6bf117d07b1f
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2021-11-06 21:47:25 +0000

    replaced ugly menu with hamburguer menu

--- js/script.js
+++ js/script.js
@@ -18,6 +18,16 @@ var btn = document.querySelectorAll(".btn");
 
 var rowOne = document.querySelector(".row-1");
 
+var menu = document.querySelector(".menu");
+
+var menuIcon = document.querySelector(".menu-icon");
+
+var cancel = document.querySelector(".cancel");
+
+var actualMenu = document.querySelector(".actual-menu");
+
+var menuClicked = false;
+
 var sticky = navbar.offsetTop;
 
 var webAppsSection = document.querySelector(".web-apps__title");
@@ -27,17 +37,47 @@ var properSticky = webAppsSection.offsetTop;
 function myStickyFunction() {
   if (window.pageYOffset >= properSticky) {
     // navbar.classList.add("sticky");
+    /*
     rowOne.classList.add("sticky");
     btn.forEach((item, i) => {
       item.classList.add("btn-full");
     });
 
+    */
+
+    menu.classList.remove("none");
     // btn.classList.add("btn-full");
   } else {
     // navbar.classList.remove("sticky");
+    /*
     rowOne.classList.remove("sticky");
     btn.forEach((item, i) => {
       item.classList.remove("btn-full");
     });
+
+    */
+
+    menu.classList.add("none");
   }
 }
+
+// Hamburger menu
+menuIcon.addEventListener("click", function menuHamburguer(e) {
+  e.preventDefault();
+
+  menu.classList.remove("none");
+
+  menuIcon.classList.add("none");
+
+  menuClicked = true;
+
+  actualMenu.classList.remove("none");
+});
+
+cancel.addEventListener("click", function toggleMenu(e) {
+  e.preventDefault();
+
+  menuIcon.classList.remove("none");
+
+  actualMenu.classList.add("none");
+});
